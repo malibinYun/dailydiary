6244112d9ee69be93ae353445136e1c4577c0007
Malibin <nightmare73@naver.com>
2019-09-07 23:21:39 +0900

    이커밋이 디폴트 이미지 enum클래스 작성한거... 이전에 대체 무슨커밋이엇던거지

--- app/src/main/java/com/wonder/dailydiary/util/DefaultImage.kt
+++ app/src/main/java/com/wonder/dailydiary/util/DefaultImage.kt
@@ -1,13 +1,35 @@
 package com.wonder.dailydiary.util
 
+import androidx.annotation.DrawableRes
+import com.wonder.dailydiary.R
+
 /**
  * Created By Yun Hyeok
  * on 9월 06, 2019
  */
 
-class DefaultImage {
+enum class DefaultImage(val id: Int, @DrawableRes val resource: Int) {
+    SUNNY1(101, R.drawable.default_cover_sunny01),
+    SUNNY2(102, R.drawable.default_cover_sunny02),
+    SUNNY3(103, R.drawable.default_cover_sunny03),
 
-    fun asdf(){
+    CLOUDY1(201, R.drawable.default_cover_cloudy01),
+    CLOUDY2(202, R.drawable.default_cover_cloudy02),
+    CLOUDY3(203, R.drawable.default_cover_cloudy03),
 
+    RAINY1(301, R.drawable.default_cover_rainy01),
+    RAINY2(302, R.drawable.default_cover_rainy02),
+    RAINY3(303, R.drawable.default_cover_rainy03),
+
+    SNOWY1(401, R.drawable.default_cover_snowy01),
+    SNOWY2(402, R.drawable.default_cover_snowy02),
+    SNOWY3(403, R.drawable.default_cover_snowy03);
+
+    companion object {
+        fun findById(id: Int): DefaultImage {
+            return values().first { it.id == id }
+        }
     }
+
 }
+
--- app/src/test/java/com/wonder/dailydiary/util/DefaultImageTest.kt
+++ app/src/test/java/com/wonder/dailydiary/util/DefaultImageTest.kt
@@ -1,9 +1,25 @@
 package com.wonder.dailydiary.util
 
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Created By Yun Hyeok
  * on 9월 07, 2019
  */
-class DefaultImageTest
+class DefaultImageTest {
+
+
+    @Test
+    fun `아이디 가지고 제대로 찾는지 확인`() {
+        // given
+        val defaultImage = DefaultImage.RAINY2
+
+        // when
+        val testImage = DefaultImage.findById(302) // Rainy2 의 실제 아이디 정수 값
+
+        // then
+        assertEquals(defaultImage, testImage)
+    }
+
+}
